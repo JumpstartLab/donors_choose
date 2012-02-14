6037a07d273284c49dbb92840bbd21bc1bf7b7b5
Steve Klabnik <steve@steveklabnik.com>
2012-02-14 14:51:29 -0500

    vcr + webmock == awesome

--- donors_choose.gemspec
+++ donors_choose.gemspec
@@ -18,4 +18,6 @@ Gem::Specification.new do |gem|
   gem.add_runtime_dependency "json"
 
   gem.add_development_dependency "rspec"
+  gem.add_development_dependency "vcr"
+  gem.add_development_dependency "webmock"
 end
--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,7 +1,20 @@
 $:.unshift("lib")
 require 'donors_choose/request'
 
+require 'vcr'
+require 'webmock'
+VCR.config do |c|
+  c.stub_with :webmock
+  c.cassette_library_dir = 'cassettes'
+end
+
+RSpec.configure do |config|
+  config.extend VCR::RSpec::Macros
+end
+
 describe DonorsChoose::Request do
+  use_vcr_cassette
+
   it "is able to request data from the API" do
     DonorsChoose.api_key = "DONORSCHOOSE"
 
