7386322304abadfd0167fbad1bc7db3b66742fbb
Steve Klabnik <steve@steveklabnik.com>
2012-02-14 15:12:05 -0500

    refactor specs

--- spec/donors_choose_spec.rb
+++ spec/donors_choose_spec.rb
@@ -11,25 +11,21 @@ describe DonorsChoose do
 
   context "requesting data" do
     let(:test_title) { "A test project" }
-    let(:request) {
-      double(:fetch => %Q/{"proposals":  [{"title": "#{test_title}"}]}/)
-    }
 
-    let(:make_request) {
-      DonorsChoose::Request.should_receive(:new).with(@request_args).and_return(request)
-    }
+    def mock_request(args)
+      request = double(:fetch => %Q/{"proposals":  [{"title": "#{test_title}"}]}/)
+      DonorsChoose::Request.should_receive(:new).with(args).and_return(request)
+    end
 
     it "is able to find projects near me" do
-      @request_args = {:centerLat => "40.4405556", :centerLong => "-79.9961111"}
-      make_request
+      mock_request(:centerLat => "40.4405556", :centerLong => "-79.9961111")
 
       projects = DonorsChoose.projects_near_me("40.4405556", "-79.9961111")
       projects.first.title.should eql(test_title)
     end
 
     it "is able to find projects by zip code" do
-      @request_args = {:keyword => "15232"}
-      make_request
+      mock_request(:keyword => "15232")
 
       projects = DonorsChoose.projects_by_zip("15232")
       projects.first.title.should eql("A test project")
