6dc608dfde70979e92f9a656275bc56609d29a4f
Steve Klabnik <steve@steveklabnik.com>
2012-02-14 12:57:02 -0500

    Move to OpenStruct

--- lib/donors_choose.rb
+++ lib/donors_choose.rb
@@ -1,17 +1,16 @@
 require 'donors_choose/version'
 require 'donors_choose/request'
 require 'json'
+require 'ostruct'
 
 module DonorsChoose
   extend self
   attr_accessor :api_key
 
-  Data = Struct.new(:title)
-
   def projects_near_me(latitude, longitude)
     request = Request.new(:centerLat => latitude, :centerLong => longitude)
     data = JSON.parse(request.fetch)["proposals"]
 
-    data.collect{|datum| Data.new(datum["title"])}
+    data.collect {|datum| OpenStruct.new(datum)}
   end
 end
