ef8c64ce2236607088b6ae4808ddfae1301c7d51
Steve Klabnik <steve@steveklabnik.com>
2012-02-14 21:16:43 -0500

    I should be using eq, not eql

--- spec/donors_choose_spec.rb
+++ spec/donors_choose_spec.rb
@@ -5,7 +5,7 @@ describe DonorsChoose do
   it "is able to have an API key set" do
     key = "this isn't an API key"
     DonorsChoose.api_key = key
-    DonorsChoose.api_key.should eql(key)
+    DonorsChoose.api_key.should eq(key)
   end
 
   context "requesting data" do
