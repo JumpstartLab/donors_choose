f217ee0c73008db6342110359a1caba2d86e67bd
Vasiliy Ermolovich <younash@gmail.com>
2012-02-16 21:57:32 +0300

    use described_class method when it possible

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,8 +1,8 @@
-$:.unshift("lib")
 require 'donors_choose/request'
 
 require 'vcr'
 require 'webmock'
+
 YAML_SERIALIZATION_VERSION = RUBY_VERSION =~ /1\.8/ ? '1.8' : 'not_1.8'
 VCR.config do |c|
   c.cassette_library_dir = File.join(File.dirname(__FILE__), '..', 'cassettes', YAML_SERIALIZATION_VERSION)
@@ -19,8 +19,8 @@ describe DonorsChoose::Request do
   it "is able to request data from the API" do
     DonorsChoose.api_key = "DONORSCHOOSE"
 
-    request = DonorsChoose::Request.new({:state => "NC",
-                                         :community => "1007:3"})
+    request = described_class.new({:state => "NC",
+                                   :community => "1007:3"})
     request.process.should_not be_nil
   end
 end
